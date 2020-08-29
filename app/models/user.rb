7c331b2d513e04730aa348d560256628a587c6d8
iamcid <iamcid17@gmail.com>
2020-08-29 00:43:37 -0400

    Allows for user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :reviewed_baby_foods, through: :reviews, source: :baby_food
 
     has_many :baby_foods
+
+    has_secure_password
+
+    validate :username, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get '/', to: 'sessions#welcome'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :reviews
   resources :baby_foods
   resources :categories
