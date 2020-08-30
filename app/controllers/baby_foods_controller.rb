914b0706fc0fe8b7324c87d057dcdceabee1c010
iamcid <iamcid17@gmail.com>
2020-08-30 00:07:04 -0400

    adds links, forms, views and controller actions for baby food

--- app/controllers/baby_foods_controller.rb
+++ app/controllers/baby_foods_controller.rb
@@ -1,2 +1,26 @@
 class BabyFoodsController < ApplicationController
+    def new
+        @baby_food = BabyFood.new
+        @baby_food.build_category
+    end
+
+    def create
+        @baby_food = BabyFood.new(baby_food_params)
+        @baby_food.user_id = session[:user_id]
+
+        if @baby_food.save
+            flash[:message] = "Successful creation!"
+            redirect_to baby_food_path(@baby_food)
+        else
+            flash[:message] = "Unsuccessful signup! Please try again!"
+            @baby_food.build_user
+            render :new
+        end
+    end
+
+    private
+
+    def baby_food_params
+        params.require(:baby_food).permit(:flavor, :description, :category_id, category_attributes: [:name])
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             flash[:message] = "Invalid credentials. Please try again."
